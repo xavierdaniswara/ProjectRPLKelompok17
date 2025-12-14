dc6cb1c2383680b6ff105926961022cc6a896366
xavierdaniswara <xavier.daniswara@ui.ac.id>
2025-12-14 15:41:12 +0700

    Backend sdh bisa dijalankan

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default;
 const cors = require('cors');
 const connectDB = require('./src/config/db.config');
 const authRoutes = require('./routes/auth');
